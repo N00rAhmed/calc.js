7f13bb27a7de4b2ca058cfb73c0b66a203576c15
Noor Ahmed <86896171+N00rAhmed@users.noreply.github.com>
2022-01-11 10:10:36 +0000

    Add files via upload

---
+++ j1.js
@@ -0,0 +1,24 @@
+const prompt = require("prompt-sync")();
+
+// Making anything using current knowledge without using google
+// later with better idea of what u wanna make then use stackoverflow
+
+const symbol = prompt('Enter operator +, -, * or / : ');
+const num1 = parseFloat(prompt('Enter first number: '));
+const num2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+if (symbol == '+') {
+    result = num1 + num2;
+}
+else if (symbol == '-') {
+    result = num1 - num2;
+}
+else if (symbol == '*'){
+    result = num1 * num2;
+}
+else{
+    result = num1 / num2;
+}
+document.write(`${num1} ${symbol} ${num2} = ${result}`);
